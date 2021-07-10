a148a4f8d1afb173d161741a1e868ca7a3f336d6
Swati Shikha <72434791+ShikhaSwati60@users.noreply.github.com>
2021-07-10 13:55:49 +0530

    Add files via upload

---
+++ threaded_bst.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    struct node *left, *right;
+    int info;
+    boolean lthread,rthread;
+};
+
+struct node *insert(struct node*root,int val)
+{
+
+};
